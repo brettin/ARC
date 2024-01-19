1ac4316f564d3b695b3547c8f58b354dd91a5341
Thomas Brettin <brettin@anl.gov>
2024-01-19 13:08:27 -0600

    set default CUDA_VISIBLE_DEVICES to 7

--- env.sh
+++ env.sh
@@ -1,9 +1,8 @@
 # Some basic stuff
-
+export PATH=$PATH:$HOME/anaconda3/bin
 export CUDA_VISIBLE_DEVICES=7
-echo $CUDA_VISIBLE_DEVICES
+echo "using CUDA_VUSIBLE_DEVICES = $CUDA_VISIBLE_DEVICES"
 
 conda activate /rbscratch/brettin/conda_envs/openai-rag-arxiv
 
 export PYTHONPATH=$PYTHONPATH:.
-echo $PYTHONPATH
